57109c4d3abc87641813eadd0980abd7fff9d33d
sujin0509 <123149418+sujin0509@users.noreply.github.com>
2023-01-26 16:51:14 +0900

    Update csj07.py

--- csj07.py
+++ csj07.py
@@ -3,45 +3,55 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-money = pd.read_csv("money_data7.csv")
+    #money = pd.read_csv("money_data7.csv")
 
-st.sidebar.success("Select a demo above.")
-
-st.title('_3년간의_ :blue[미국금리] ,:blue[한국금리],:blue[코스피지수],:blue[집값] 데이터 :sunglasses:')
-
-
-option = st.selectbox(
-    'How would you like to choice year ?',
-    ('2020', '2021', '2022'))
-
-option2 = int(option)
-
-st.write('You selected:', option)
-
-money = money[:] [money['A_YEAR']== option2]
-
-fig, ax = plt.subplots(2,2, figsize=(12,8))
-
-plt.subplot(221)
-plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
-plt.xticks(tuple(money['A_MONTH']) )
-plt.title('America Rate')
+    #st.sidebar.success("Select a demo above.")
+def  plotting_demo():
+    st.title('_3년간의_ :blue[미국금리] ,:blue[한국금리],:blue[코스피지수],:blue[집값] 데이터 :sunglasses:')
 
 
-plt.subplot(222)
-plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
-plt.xticks(tuple(money['A_MONTH']) )
-plt.title('Korea Rate')
+    option = st.selectbox(
+        'How would you like to choice year ?',
+        ('2020', '2021', '2022'))
 
-plt.subplot(223)
-plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
-plt.xticks(tuple(money['A_MONTH']) )
-plt.title('Kospi')
+    option2 = int(option)
 
-plt.subplot(224)
-plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
-plt.xticks(tuple(money['A_MONTH']) )
-plt.title('House Price')
+    st.write('You selected:', option)
 
-st.pyplot(fig)
-st.dataframe(money)
+    money = money[:] [money['A_YEAR']== option2]
+
+    fig, ax = plt.subplots(2,2, figsize=(12,8))
+
+    plt.subplot(221)
+    plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('America Rate')
+
+
+    plt.subplot(222)
+    plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('Korea Rate')
+
+    plt.subplot(223)
+    plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('Kospi')
+
+    plt.subplot(224)
+    plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+    plt.xticks(tuple(money['A_MONTH']) )
+    plt.title('House Price')
+
+    st.pyplot(fig)
+    st.dataframe(money)
+    
+    
+with st.form(key ='Form1'):
+    with st.sidebar:
+        
+        select_language = st.sidebar.radio('What do you want ?', ('line', 'bar', 'pie'))
+        
+        
+if select_language =='line':        
+    plotting_demo()      
