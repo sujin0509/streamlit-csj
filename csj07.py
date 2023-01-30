85d6c7344e591916d4bc9a9aae1a3f5bb10f9794
sujin0509 <123149418+sujin0509@users.noreply.github.com>
2023-01-30 09:12:59 +0900

    Update csj07.py

--- csj07.py
+++ csj07.py
@@ -8,6 +8,10 @@ import numpy as np
 #st.sidebar.success("Select a demo above.")
 
 def  plotting_demo():
+        
+    uploaded_file = st.file_uploader("Choose a file")
+
+    money=pd.read_csv(uploaded_file)
     money = pd.read_csv("money_data7.csv")
     option = st.selectbox(
         'How would you like to choice year ?',
